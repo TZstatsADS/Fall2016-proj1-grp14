580461df412e94eb604a3ea4de36631470acb462
sumc1992 <sen.zhuang92@gmail.com>
2016-09-17 16:30:50 -0400

    plus water, gas and electricity

--- data/data_extraction.r
+++ data/data_extraction.r
@@ -4,10 +4,12 @@ require(sqldf)
 mydata_a <- read.csv(file.choose(), header = T, sep = ",")
 mydata_b <- read.csv(file.choose(), header = T, sep = ',')
 st <- read.csv(file.choose(), header = T, sep = ',')
+mydata <- rbind(mydata_a, mydata_b)
 df <- sqldf('SELECT mydata.NOC, mydata.NP, mydata.BDSP, 
                 mydata.RMSP, mydata.RNTP, mydata.VALP, mydata.FES,
             mydata.FINCP, mydata.GRPIP, mydata.HHL, mydata.HINCP,
-            mydata.TAXP, mydata.WKEXREL, mydata.WORKSTAT, st.name, st.abbr
+            mydata.TAXP, mydata.WKEXREL, mydata.WORKSTAT, mydata.WATP,
+            mydata.GASP, mydata.FULP, st.name, st.abbr
             FROM mydata, st
             WHERE NOC >= 1
             AND mydata.ST = st.code')
