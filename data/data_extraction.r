0b8a78c0f92ae5e67245a89e3e9a47f7eb5a2dd6
sumc1992 <sen.zhuang92@gmail.com>
2016-09-17 15:40:58 -0400

    r script used to create raw RData file

---
+++ data/data_extraction.r
@@ -0,0 +1,15 @@
+require(sqldf)
+
+# Choose ss14husa.csv and ss14husb.csv
+mydata_a <- read.csv(file.choose(), header = T, sep = ",")
+mydata_b <- read.csv(file.choose(), header = T, sep = ',')
+st <- read.csv(file.choose(), header = T, sep = ',')
+df <- sqldf('SELECT mydata.NOC, mydata.NP, mydata.BDSP, 
+                mydata.RMSP, mydata.RNTP, mydata.VALP, mydata.FES,
+            mydata.FINCP, mydata.GRPIP, mydata.HHL, mydata.HINCP,
+            mydata.TAXP, mydata.WKEXREL, mydata.WORKSTAT, st.name, st.abbr
+            FROM mydata, st
+            WHERE NOC >= 1
+            AND mydata.ST = st.code')
+
+save(df, file = 'data.RData')
