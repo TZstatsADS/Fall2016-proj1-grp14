8c0eb9dad3f092f31c083687475b60980b6efb84
Tian Sheng <ts2949@columbia.edu>
2016-09-21 16:59:52 -0400

    add survey weights

--- data/data_extraction.r
+++ data/data_extraction.r
@@ -12,18 +12,35 @@ require(sqldf)
 # TAXP, Property tax
 # WKEXREL, Work experience of householder and spouse
 # WORKSTAT, Work status of householder or spouse in family households
+# WGTP, Survey weights 
 # Choose ss14husa.csv and ss14husb.csv
 mydata_a <- read.csv(file.choose(), header = T, sep = ",")
 mydata_b <- read.csv(file.choose(), header = T, sep = ',')
 st <- read.csv(file.choose(), header = T, sep = ',')
 mydata <- rbind(mydata_a, mydata_b)
-df <- sqldf('SELECT mydata.NOC, mydata.NP, mydata.BDSP, 
-                mydata.RMSP, mydata.RNTP, mydata.VALP, mydata.FES,
-            mydata.FINCP, mydata.GRPIP, mydata.HHL, mydata.HINCP,
-            mydata.TAXP, mydata.WKEXREL, mydata.WORKSTAT, mydata.WATP,
-            mydata.GASP, mydata.FULP, mydata.ELEP, st.name, st.abbr
-            FROM mydata, st
-            WHERE NOC >= 1
-            AND mydata.ST = st.code')
+cols_1<-c("NOC","NP","BDSP","RMSP","RNTP","VALP","FES","FINCP","GRPIP","HHL","HINCP",
+          "TAXP","WKEXREL","WORKSTAT","WATP","GASP","FULP",
+          "ELEP")
+cols_st<-sqldf('SELECT st.name, st.abbr
+            FROM st')
+df1<-select(mydata,one_of(cols_1),156:235)
+df<-cbind(cols_st,df1)
+df<-as.data.frame(df)
+#Using weights#
+library(survey)
+df<-svrepdesign(variables=df[,1:18], 
+                 repweights=pus_new[,19:98], type="BRR",combined.weights=TRUE,
+                 weights=df$WGTP)
+summary(df)
+
+
+#df<- sqldf('SELECT mydata.NOC, mydata.NP, mydata.BDSP, 
+#                mydata.RMSP, mydata.RNTP, mydata.VALP, mydata.FES,
+#            mydata.FINCP, mydata.GRPIP, mydata.HHL, mydata.HINCP,
+#            mydata.TAXP, mydata.WKEXREL, mydata.WORKSTAT, mydata.WATP,
+#            mydata.GASP, mydata.FULP, mydata.ELEP, st.name, st.abbr
+#            FROM mydata, st
+#            WHERE NOC >= 1
+#            AND mydata.ST = st.code')
 
 save(df, file = 'data.RData')
